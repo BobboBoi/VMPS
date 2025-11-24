28cf78acefe9f1408c2c29eb5933d28330c279be
BobboBoi <git.jurre@serrarens.nl>
2025-11-24 17:14:30 +0100

    Add non moving enemy

---
+++ src/main/java/nl/saxion/game/vmps/classes/EnemyManager.java
@@ -0,0 +1,7 @@
+package nl.saxion.game.vmps.classes;
+
+import java.util.ArrayList;
+
+public class EnemyManager {
+    public ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+}
---
+++ src/main/java/nl/saxion/game/vmps/classes/Scene.java
@@ -0,0 +1,53 @@
+package nl.saxion.game.vmps.classes;
+
+import com.badlogic.gdx.math.Vector2;
+import nl.saxion.gameapp.GameApp;
+import nl.saxion.gameapp.screens.ScalableGameScreen;
+import java.util.ArrayList;
+
+/** Wrapper class for ScalableGameScreen that allows the use of Object2D **/
+public abstract class Scene extends ScalableGameScreen {
+    protected boolean ready = false;
+
+    protected double time = 0.;
+    public final Vector2 size;
+    public final Vector2 center;
+
+    public ArrayList<Object2D> objects = new ArrayList<Object2D>();
+
+    public Scene(int worldWidth, int worldHeight) {
+        super(worldWidth, worldHeight);
+
+        this.size = new Vector2(worldWidth, worldHeight);
+        this.center = new Vector2((float) worldWidth / 2, (float) worldHeight / 2);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        if (!ready) {
+            ready();
+            this.ready = true;
+        }
+
+        this.time += delta;
+
+        // Clear previous frame
+        GameApp.clearScreen();
+
+        // Render all level objects
+        GameApp.startSpriteRendering();
+        for (Object2D o : objects) {
+            o.render(delta);
+        }
+        GameApp.endSpriteRendering();
+    }
+
+    public abstract void show();
+    public abstract void hide();
+
+    /**
+     * Called on the first frame.
+     */
+    public abstract void ready();
+}
